1d691e98a08d784b9e124447876dc7a51834fb34
SumanVastrakar <96719499+SumanVastrakar@users.noreply.github.com>
2022-08-06 17:00:14 +0530

    Edit and Delete added

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require("path");
 const mongoose = require("mongoose");
 const Campground = require("./models/campground")
 app.use(express.urlencoded({extended : true}))
+const methodOverride = require("method-override");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp",{
     useNewUrlParser : true,
@@ -21,6 +22,7 @@ db.once("open", () => {
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname,"views"))
+app.use(methodOverride("_method"));
 
 
 
@@ -50,13 +52,23 @@ app.get("/campgrounds/:id", async(req, res) =>{
     res.render("campgrounds/show", {campground})
 })
 
-// app.get("/makecampground", async(req, res) =>{
-//     const camp = new Campground({title : "My Backyard", description : "cheap camping"});
-//     await camp.save();
-//     res.send(camp)
-// })
+app.get("/campgrounds/:id/edit", async(req, res) =>{
+    const campground = await Campground.findById(req.params.id)
+    res.render("campgrounds/edit", {campground})
+})
 
+app.put("/campgrounds/:id", async(req, res) =>{
+   const {id} = req.params;
+   const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
+   res.redirect(`/campgrounds/${campground._id}`)
 
+})
+
+app.delete('/campgrounds/:id', async(req, res) =>{
+    const {id} = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect("/campgrounds");
+})
 
 
 
