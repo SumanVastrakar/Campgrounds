4400210f309eba4bd55347557ef5b998025fae9b
SumanVastrakar <96719499+SumanVastrakar@users.noreply.github.com>
2022-08-06 16:23:41 +0530

    Ampground new and create

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
 const Campground = require("./models/campground")
+app.use(express.urlencoded({extended : true}))
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp",{
     useNewUrlParser : true,
@@ -25,6 +26,28 @@ app.set("views", path.join(__dirname,"views"))
 
 app.get("/", (req, res) =>{
     res.render("home")
+})
+
+app.get("/campgrounds", async(req, res) =>{
+    const campgrounds = await Campground.find({});
+    res.render("campgrounds/index",{campgrounds})
+})
+
+
+app.get("/campgrounds/new", (req, res) =>{
+    res.render("campgrounds/new")
+})
+
+app.post("/campgrounds", async(req, res) =>{
+   const campground = new  Campground(req.body.campground)
+   await campground.save();
+   res.redirect(`/campgrounds/${campground._id}`)
+
+})
+
+app.get("/campgrounds/:id", async(req, res) =>{
+    const campground = await Campground.findById(req.params.id)
+    res.render("campgrounds/show", {campground})
 })
 
 // app.get("/makecampground", async(req, res) =>{
